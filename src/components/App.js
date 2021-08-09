12ac328420aec06d0406d57e51082633d751def7
sunnythelucky <sunnyparkk@gmail.com>
2021-08-09 15:56:40 -0400

    make server

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,30 @@
-import { StrictMode, useState } from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-
-import SearchParams from "./SearchParams";
+import { useState, StrictMode } from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import Details from "./Details";
+import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  const theme = useState("pink");
-  // const themeHook = useState("darkblue");
+  const theme = useState("darkblue");
   return (
-    <ThemeContext.Provider value={theme}>
-      <div>
-        <Router>
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <div>
           <header>
-            <Link to="/">
-              <h1 id="my-brand">Adopt me!</h1>
-            </Link>
+            <Link to="/">Adopt Me!</Link>
           </header>
           <Switch>
             <Route path="/details/:id">
               <Details />
-              {/* ThemeContext.Consumer  
-              because it's a class component.  */}
             </Route>
             <Route path="/">
               <SearchParams />
             </Route>
           </Switch>
-        </Router>
-      </div>
-    </ThemeContext.Provider>
+        </div>
+      </ThemeContext.Provider>
+    </StrictMode>
   );
 };
 
-render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+export default App;
